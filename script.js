d2270b45fdc641fcc20f756d8124d34a7c0d9f0c
Raquel Moreira <raquelmoreirasilva0215@gmail.com>
2026-08-25 20:06:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+//CRIA UM NOVO ELEMENTO NA PÁGINA
+
+const botaoVerMais = document.getElementById("btn-ver-mais");
+
+const conteudoExtra = document.getElementById("conteudo-extra");
+
+botaoVerMais.addEventListener("click", function () {
+
+    const novoParagrafo = document.createElement("p");
+
+    novoParagrafo.textContent =
+        "Estou desenvolvendo projetos para praticar meus conhecimentos e construir meu portfólio profissional.";
+
+    conteudoExtra.appendChild(novoParagrafo);
+
+});
+
+
