59afe76339991ac3b9e7866500cc81c2487dddce
Raquel Moreira <raquelmoreirasilva0215@gmail.com>
2026-09-03 19:09:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,21 @@
 
-// const botaoVerMais = document.getElementById("btn-ver-mais");
-
-// const conteudoExtra = document.getElementById("conteudo-extra");
-
-// botaoVerMais.addEventListener("click", function () {
-
-//     const novoParagrafo = document.createElement("p");
-
-//     novoParagrafo.textContent =
-//         "Estou desenvolvendo projetos para praticar meus conhecimentos e construir meu portfólio profissional.";
-
-//     conteudoExtra.appendChild(novoParagrafo);
-
-// });
-
-
-// const sobre = document.getElementById("sobre");
-// const novoParagrafo = document.createElement("p");
-// novoParagrafo.textContent = "Também tenho interesse em acessibilidade e experiência do usuário.";
-// sobre.appendChild(novoParagrafo);
-
-
-
-//  const botaoEnviar = document.getElementById("btn-enviar");
-
-// const conteudoExtra = document.getElementById("conteudo-extra");
-
-// botaoEnviar.addEventListener("click", function () {
-
-//     const novoParagrafo = document.createElement("p");
-
-//     novoParagrafo.textContent =
-//         "Enviado com sucesso!";
-
-//     conteudoExtra.appendChild(novoParagrafo);
-
-// });
-
-
-
-//formulario.addEventListener("submit", function(event) {
-
-//    event.preventDefault();
-
-//    console.log("Nome:", nome.value);
-//    console.log("E-mail:", email.value);
-//    console.log("Assunto:", assunto.value);
-//    console.log("Mensagem:", mensagem.value);
-
-//});
-
-
-
-
-// contato.addEventListener("submit", function(event) {
-
-//     event.preventDefault();
-
-//     console.log("nome:", nome.value);
-//     console.log("e-mail:", email.value);
-
-// });
-
-// const mensagemSucesso =
-//    document.getElementById("mensagem-sucesso");
-// mensagemSucesso.textContent = "Mensagem enviada com sucesso!";
-
-
+const botaoVerMais = document.getElementById("btn-ver-mais");
+const conteudoExtra = document.getElementById("conteudo-extra");
 
 let mostrandoMais = false;
 
 botaoVerMais.addEventListener("click", function () {
 
-   if (mostrandoMais === false) {
+    if (mostrandoMais === false) {
 
-       const novoParagrafo = document.createElement("p");
+        const novoParagrafo = document.createElement("p");
 
         novoParagrafo.textContent =
             "Estou desenvolvendo projetos para praticar meus conhecimentos e construir meu portfólio profissional.";
 
-      conteudoExtra.appendChild(novoParagrafo);
+        conteudoExtra.appendChild(novoParagrafo);
 
-      botaoVerMais.textContent = "Ver menos";
+        botaoVerMais.textContent = "Mostrar menos";
 
         mostrandoMais = true;
 
@@ -89,9 +23,11 @@ botaoVerMais.addEventListener("click", function () {
 
         conteudoExtra.innerHTML = "";
 
-        botaoVerMais.textContent = "Ver mais";
+        botaoVerMais.textContent = "Conheça mais sobre mim";
 
         mostrandoMais = false;
     }
 
 });
+
+
